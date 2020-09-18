522b175ade4789b88226c12334fb5ac84c73f64a
Hitesh <hitgo17@gmail.com>
2020-09-18 05:57:49 +0530

    feat:published first npm package 🥳
    Signed-off-by: Hitesh <hitgo17@gmail.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+##Env Template maker
---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+fs.readFile('.env', 'utf8', (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  let tempData = [];
+  let tempEnv = '';
+  data = data.split('\n');
+  data = data.forEach((element) => {
+    let key = element.split('=')[0];
+    tempData.push(key.concat(`=#Your ${key} here\n`));
+  });
+  tempData.forEach((element) => {
+    tempEnv = `${tempEnv}${element}`;
+  });
+
+  fs.writeFile('.env.template', tempEnv, function (err) {
+    if (err) throw err;
+    console.log('.env.template file created successfully');
+  });
+});
